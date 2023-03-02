4d60835d7559cf28f4dd2ddede9e5491e8694b56
PreetiNegi2709 <105388761+PreetiNegi2709@users.noreply.github.com>
2023-03-02 20:00:09 +0530

    pass data via props
    making components reusable with the dynamic data

--- expensetracker/src/components/ExpenseItem.js
+++ expensetracker/src/components/ExpenseItem.js
@@ -1,20 +1,29 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    // want dynamic data (from user) but here using dummmy data
-    const expenseDate = new Date(2020,2,21);
+// function ExpenseItem(date,title,amount) --- wronge not 3 para
+
+// WE get 1 parameter per component  => 
+// 1 parameter = is object that holds all attributes (here Date,title,amount) as properties
+// parameter name can be anything = but prefer props(to be clear this is obj which holds all the att for custom comp)
+// we get (key,value) pair in props obj
+// keys = attribute name (here title,date,amount)
+// values = values set in the App.js inside custom comp
+
+function ExpenseItem(props) 
+
+{
+
     // now expenseData return date obj so to display
     // we have to first convert it to string use :-
     // .toISOString() : build in method conv date obj to string
-    const expenseTitle = "Car Insurance";
-    const expenseAmount = 260.67;
+
   return (
     <div className="expense-item">
       {/* <div>{expenseDate}</div> won't display the site will crash as the date is obj have ti conv it to string */}
-      <div> {expenseDate.toISOString()} </div>
+      <div> { props.date.toISOString() } </div>
       <div className="expense-item__description">
-        <h2> {expenseTitle} </h2>
-        <div className="expense-item__price"> {expenseAmount} </div>
+        <h2> {props.title} </h2>
+        <div className="expense-item__price"> {props.amount} </div>
       </div>
     </div>
   );
