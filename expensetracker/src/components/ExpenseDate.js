952e21bc6d28c4753c489b58a91d22f1b7e68272
PreetiNegi2709 <105388761+PreetiNegi2709@users.noreply.github.com>
2023-03-02 21:42:35 +0530

    split component into multiple components

---
+++ expensetracker/src/components/ExpenseDate.js
@@ -0,0 +1,19 @@
+import './ExpenseDate.css';
+
+function ExpenseDate(props) {
+  // props.date.toISOString() = will convert date object to string (2020-08-13T18:30:00.000Z)
+  // .toLocaleString() method returns a string with a language-sensitive (eng) representation of date
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const date = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{date}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
--- expensetracker/src/components/ExpenseItem.js
+++ expensetracker/src/components/ExpenseItem.js
@@ -1,20 +1,11 @@
 import "./ExpenseItem.css";
+import ExpenseDate from'./ExpenseDate'
 
 function ExpenseItem(props) {
-    // props.date.toISOString() = will convert date object to string (2020-08-13T18:30:00.000Z)
-    // .toLocaleString() method returns a string with a language-sensitive (eng) representation of date
-
-  const month = props.date.toLocaleString('en-US',{month:'long'}) ;
-  const date = props.date.toLocaleString('en-US',{day:'2-digit'}) ;
-  const year = props.date.getFullYear() ;
-
   return (
     <div className="expense-item">
-      <div> 
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{date}</div>
-      </div>
+        {/* comp inside com= split mult comp */}
+        <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2> {props.title} </h2>
         <div className="expense-item__price"> {props.amount} </div>
