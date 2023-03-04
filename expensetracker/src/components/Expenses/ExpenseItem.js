72be0a68032da7bbb5b4ac9594ccdf88bc0da0a9
PreetiNegi2709 <105388761+PreetiNegi2709@users.noreply.github.com>
2023-03-04 13:49:18 +0530

    state practice

--- expensetracker/src/components/Expenses/ExpenseItem.js
+++ expensetracker/src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,25 @@
 import "./ExpenseItem.css";
 import ExpenseDate from'./ExpenseDate'
 import Card from '../UI/Card'
+import React, { useState } from 'react';
 
-// function ExpenseItem(props) {
 const ExpenseItem = (props) => {
+  // useState(props.title);
+  const [title, setTitle] = useState(props.title)
+
+  const clickHandle = () => {
+    // console.log("button is clicked")
+    setTitle("updated title")
+  }
+
   return (
-    // <div className="expense-item">
     <Card className="expense-item">
-        {/* comp inside com= split mult comp */}
         <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2> {props.title} </h2>
+        <h2> {title} </h2>
         <div className="expense-item__price"> ${props.amount} </div>
       </div>
+      <button onClick={clickHandle}>click me</button>
     </Card>
   );
 }
